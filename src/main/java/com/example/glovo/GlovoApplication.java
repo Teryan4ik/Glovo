2d46a1860ff596bbaf79d5d6bcbd75bdccc6c44b
Jollly_Rodger <andrey.teryannikov@gmail.com>
2023-05-01 14:54:09 +0300

    Initial commit

---
+++ src/main/java/com/example/glovo/GlovoApplication.java
@@ -0,0 +1,10 @@
+package com.example.glovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoApplication.class, args
