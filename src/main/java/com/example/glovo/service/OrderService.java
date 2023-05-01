c2e7f29199a11562272fa4baab8eb3dc317a3068
Jollly_Rodger <andrey.teryannikov@gmail.com>
2023-05-01 16:54:02 +0300

    Add controller

--- src/main/java/com/example/glovo/service/OrderService.java
+++ src/main/java/com/example/glovo/service/OrderService.java
@@ -6,17 +6,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class OrderService {
     private final List<Order> orders = new ArrayList<>();
 
     public OrderService() {
-        Product apple = Product.builder().id(1).cost(15).name("Apple").build();
-        List <Product> products = List.of(apple);
-        orders.add(Order.builder().cost(15).date("03-11-2022").id(1).products(products).build());
+        Product apple = Product.builder().id(1).cost(5).name("Apple").build();
+        Product orange = Product.builder().id(2).cost(10).name("Orange").build();
+
+        List<Product> products = new ArrayList<>();
+        List<Product> products2 = new ArrayList<>();
+        products.add(apple);
+        products2.add(orange);
+
+        orders.add(Order.builder().cost(15).date("01-01-2001").id(1).products(products).build());
+        orders.add(Order.builder().cost(30).date("02-02-2002").id(2).products(products2).build());
     }
 
-    private Order get(int id) {
+    public Order get(int id) {
         for (Order order : orders) {
             if (order.getId() == id) {
                 return order;
@@ -25,7 +33,7 @@ public class OrderService {
         return null;
     }
 
-    private List<Order> getAll() {
+    public List<Order> getAll() {
         return orders;
     }
 
