87e3e831bd4540d0322553bb6a157b5599aaa652
Jollly_Rodger <andrey.teryannikov@gmail.com>
2023-05-09 17:31:31 +0300

    Changes in ProductRepository

--- src/main/java/com/example/glovo/repository/OrderRepository.java
+++ src/main/java/com/example/glovo/repository/OrderRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends CrudRepository<OrderEntity, Integer> {
-
+public interface OrderRepository extends CrudRepository <OrderEntity, Integer> {
     List<OrderEntity> findAll();
 }
